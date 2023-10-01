5d6b116164b1e62fbb1c7dd46ebbcb5c21628fd2
Dark-Knight-16 <alexghoul45@gmail.com>
2023-10-01 17:10:35 +0530

    made changes in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+// components
+import RootLayout from './RootLayout.jsx'
+
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+    
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
